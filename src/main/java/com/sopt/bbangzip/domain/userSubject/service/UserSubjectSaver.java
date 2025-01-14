89fac7b77ba0ba7f23b54f207651c440ec7f762f
choyeongju <jjudy1301@naver.com>
2025-01-15 01:14:35 +0900

    :recycle: [refac] #28 UserSubject 관련 서비스 클래스들

---
+++ src/main/java/com/sopt/bbangzip/domain/userSubject/service/UserSubjectSaver.java
@@ -0,0 +1,16 @@
+package com.sopt.bbangzip.domain.userSubject.service;
+
+import com.sopt.bbangzip.domain.userSubject.entity.UserSubject;
+import com.sopt.bbangzip.domain.userSubject.repository.UserSubjectRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class UserSubjectSaver {
+    private final UserSubjectRepository userSubjectRepository;
+
+    public UserSubject save(UserSubject userSubject) {
+        return userSubjectRepository.save(userSubject);
+    }
+}
