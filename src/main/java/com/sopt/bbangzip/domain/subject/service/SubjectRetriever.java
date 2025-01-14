0d1a945fe22f98eee07661368ee4728ed9fc9894
choyeongju <jjudy1301@naver.com>
2025-01-15 01:14:25 +0900

    :recycle: [refac] #28 Subject 관련 서비스 클래스들

--- src/main/java/com/sopt/bbangzip/domain/subject/service/SubjectRetriever.java
+++ src/main/java/com/sopt/bbangzip/domain/subject/service/SubjectRetriever.java
@@ -1,10 +1,7 @@
 package com.sopt.bbangzip.domain.subject.service;
 
-import com.sopt.bbangzip.common.exception.base.NotFoundException;
-import com.sopt.bbangzip.common.exception.code.ErrorCode;
-import com.sopt.bbangzip.domain.user.repository.UserRepository;
+import com.sopt.bbangzip.domain.subject.repository.SubjectRepository;
 import com.sopt.bbangzip.domain.userSubject.entity.UserSubject;
-import com.sopt.bbangzip.domain.userSubject.repository.UserSubectRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -12,25 +9,10 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class SubjectRetriever {
 
-    private final UserRepository userRepository;
-    private final UserSubectRepository userSubectRepository;
+    private final SubjectRepository subjectRepository;
 
-    // 유저 존재 여부 확인
-    public void validateUserExists(Long userId) {
-        if (!userRepository.existsById(userId)) {
-            throw new NotFoundException(ErrorCode.UNAUTHORIZED);
-        }
-    }
-
-    // UserSubject 조회 및 생성
-    public UserSubject findOrCreateUserSubject(Long userId, int year, String semester) {
-        return userSubectRepository.findByUser_IdAndYearAndSemester(userId, year, semester)
-                .orElseGet(() -> userSubectRepository.save(
-                        new UserSubject(
-                                userRepository.findById(userId).orElseThrow(() -> new NotFoundException(ErrorCode.UNAUTHORIZED)),
-                                year,
-                                semester
-                        )
-                ));
+    // userSubject subjectName 에 해당하는 과목(Subject) 가 존재하는지 확인하는 메서드
+    public boolean existsByUserSubjectAndSubjectName(UserSubject userSubject, String subjectName){
+        return subjectRepository.existsByUserSubjectAndSubjectName(userSubject, subjectName);
     }
 }
--- src/main/java/com/sopt/bbangzip/domain/subject/service/SubjectService.java
+++ src/main/java/com/sopt/bbangzip/domain/subject/service/SubjectService.java
@@ -1,7 +1,14 @@
 package com.sopt.bbangzip.domain.subject.service;
 
+import com.sopt.bbangzip.common.exception.base.DuplicateSubjectException;
+import com.sopt.bbangzip.common.exception.code.ErrorCode;
 import com.sopt.bbangzip.domain.subject.api.dto.request.SubjectCreateDto;
+import com.sopt.bbangzip.domain.subject.entity.Subject;
+import com.sopt.bbangzip.domain.user.entity.User;
+import com.sopt.bbangzip.domain.user.service.UserRetriever;
 import com.sopt.bbangzip.domain.userSubject.entity.UserSubject;
+import com.sopt.bbangzip.domain.userSubject.service.UserSubjectRetriever;
+import com.sopt.bbangzip.domain.userSubject.service.UserSubjectSaver;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -11,23 +18,34 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class SubjectService {
 
+    private final UserRetriever userRetriever;
+
     private final SubjectRetriever subjectRetriever;
     private final SubjectSaver subjectSaver;
 
+    private final UserSubjectRetriever userSubjectRetriever;
+    private final UserSubjectSaver userSubjectSaver;
+
     public void createSubject(Long userId, SubjectCreateDto subjectCreateDto) {
-        // 1. 유저 존재 확인
-        subjectRetriever.validateUserExists(userId);
 
-        // 2. UserSubject 조회 또는 생성
-        UserSubject userSubject = subjectRetriever.findOrCreateUserSubject(
-                userId, subjectCreateDto.year(), subjectCreateDto.semester()
-        );
+        User user = userRetriever.findByUserId(userId); // 유저 존재 여부까지 확인 가능
 
-        // 3. 동일 학기 내 중복된 과목명 확인
-        subjectSaver.validateDuplicateSubject(userSubject, subjectCreateDto.subjectName());
+        // UserSubject 조회 또는 생성
+        UserSubject userSubject = userSubjectRetriever.findByUserIdAndYearAndSemester(userId, subjectCreateDto.year(), subjectCreateDto.semester());
+        userSubjectSaver.save(userSubject);
 
-        // 4. 과목 저장
-        subjectSaver.saveSubject(userSubject, subjectCreateDto);
+        // 동일 학기 내 중복된 과목 있으면 예외 던지기
+        if (subjectRetriever.existsByUserSubjectAndSubjectName(userSubject, subjectCreateDto.subjectName())) {
+            throw new DuplicateSubjectException(ErrorCode.DUPLICATED_SUBJECT);
+        }
+
+        // 중복된 과목명 없으면 subject 만들고
+        Subject subject = Subject.builder()
+                .userSubject(userSubject)
+                .subjectName(subjectCreateDto.subjectName())
+                .build();
+
+        // subject 자체를 넘겨줘서 저장하기
+        subjectSaver.save(subject);
     }
-
 }
