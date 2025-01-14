9ef19e41be1ebc61a4b7fef2927e86aa42725426
yeeun0702 <kimyeeun0702@naver.com>
2025-01-14 15:19:16 +0900

    ✨ [feat] #22 과목 추가하기 API 구현

--- src/main/java/com/sopt/bbangzip/domain/user/repository/UserRepository.java
+++ src/main/java/com/sopt/bbangzip/domain/user/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.sopt.bbangzip.domain.user.repository;
 
 import com.sopt.bbangzip.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
--- src/main/java/com/sopt/bbangzip/domain/userSubject/repository/UserSubectRepository.java
+++ src/main/java/com/sopt/bbangzip/domain/userSubject/repository/UserSubectRepository.java
@@ -1,7 +1,16 @@
 package com.sopt.bbangzip.domain.userSubject.repository;
 
+import com.sopt.bbangzip.domain.user.entity.User;
 import com.sopt.bbangzip.domain.userSubject.entity.UserSubject;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface UserSubectRepository extends JpaRepository<UserSubject, Long> {
+    Optional<UserSubject> findByUser_IdAndYearAndSemester(
+            Long userId,  int year, String semester
+    );
 }
+
