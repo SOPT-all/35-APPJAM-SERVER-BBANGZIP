48037cea3f2b2f3a3804b570c7cc6675457feaef
choyeongju <jjudy1301@naver.com>
2025-01-14 22:47:25 +0900

    :sparkles: [feat] #17 TestController 에도 테스트 토큰 발급 시 유저의 온보딩 여부 가져오도록 수정

--- src/main/java/com/sopt/bbangzip/test/TestController.java
+++ src/main/java/com/sopt/bbangzip/test/TestController.java
@@ -2,6 +2,9 @@ package com.sopt.bbangzip.test;
 
 import com.sopt.bbangzip.common.annotation.UserId;
 import com.sopt.bbangzip.domain.token.api.JwtTokensDto;
+import com.sopt.bbangzip.domain.user.entity.User;
+import com.sopt.bbangzip.domain.user.service.UserRetriever;
+import com.sopt.bbangzip.fegin.kakao.service.KakaoService;
 import com.sopt.bbangzip.security.jwt.JwtTokenProvider;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -9,30 +12,42 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/test")
 @RequiredArgsConstructor
 public class TestController {
 
     private final JwtTokenProvider jwtTokenProvider;
+    private final KakaoService kakaoService;
+    private final UserRetriever userRetriever;
 
-    @GetMapping("/success")
+    @GetMapping("/api/v1/test/success")
     public ResponseEntity<TestDto> testSuccess(){
         return ResponseEntity.ok(TestDto.builder().content("얼른 자고싶어..").build());
     }
 
-    @GetMapping("/token/{userId}")
+    @GetMapping("/api/v1/test/token/{userId}")
     public ResponseEntity<JwtTokensDto> testToken(
             @PathVariable final Long userId
     ) {
-        JwtTokensDto tokens = jwtTokenProvider.issueTokens(userId);
+        // 유저의 온보딩 여부를 가져오는 로직 추가
+        User user = userRetriever.findByUserId(userId);
+        boolean isOnboardingComplete = user.getIsOnboardingComplete();
+        JwtTokensDto tokens = jwtTokenProvider.issueTokens(userId, isOnboardingComplete);
         return ResponseEntity.ok(tokens);
     }
 
-    @GetMapping("/security")
+    @GetMapping("/api/v1/test/security")
     public ResponseEntity<TestDto> testSecurity(
             @UserId final Long userId,
             @Valid @RequestBody final TestSecurity testSecurity
     ) {
         return ResponseEntity.ok(TestDto.builder().content(testSecurity.name() + " " + userId).build());
+    }
+
+    // 클라이언트(웹브라우저) 측에서 로그인 요청 시 코드 받아오는 테스트 컨트롤러
+    @GetMapping("/callback")
+    public ResponseEntity<String> callback(
+            @RequestParam final String code
+    ) {
+        return ResponseEntity.ok("Code received: " + code);
     }
 }
