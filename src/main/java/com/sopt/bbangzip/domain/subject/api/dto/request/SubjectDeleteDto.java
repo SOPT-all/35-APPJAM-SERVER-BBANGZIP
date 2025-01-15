c8d13ba49cf682de0f6aadc9d3ef6c8aee84cb29
yeeun0702 <kimyeeun0702@naver.com>
2025-01-15 17:32:51 +0900

    ✨ [feat] #24 SubjectRemover와 SubjectService 역할 분리

--- src/main/java/com/sopt/bbangzip/domain/subject/api/dto/request/SubjectDeleteDto.java
+++ src/main/java/com/sopt/bbangzip/domain/subject/api/dto/request/SubjectDeleteDto.java
@@ -2,4 +2,9 @@ package com.sopt.bbangzip.domain.subject.api.dto.request;
 
 import java.util.List;
 
-public record SubjectDeleteDto(int year, String semester, List<Long> subjectIds) {}
+public record SubjectDeleteDto(
+        int year,
+        String semester,
+        List<Long> subjectIds
+) {
+}
