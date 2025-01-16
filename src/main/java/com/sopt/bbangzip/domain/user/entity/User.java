11b69db4abe02894ca312de217759c87c5e4a66f
choyeongju <jjudy1301@naver.com>
2025-01-17 06:33:47 +0900

    :sparkles: [feat] #44 User 에 필요한 필드 추가 및 불필요 코드 삭제

--- src/main/java/com/sopt/bbangzip/domain/user/entity/User.java
+++ src/main/java/com/sopt/bbangzip/domain/user/entity/User.java
@@ -34,8 +34,11 @@ public class User {
     @Column(name = UserTableConstants.COLUMN_IS_ONBOARDING_COMPLETE, nullable = false)
     private Boolean isOnboardingComplete;
 
+    @Column(name = UserTableConstants.COLUMN_USER_POINT, nullable = false)
+    private int point = 0;
+
     @Column(name = UserTableConstants.COLUMN_USER_LEVEL, nullable = false)
-    private Long userLevel;
+    private int userLevel;
 
     @Column(name = UserTableConstants.COLUMN_CREATED_AT, nullable = false, updatable = false)
     private LocalDateTime createdAt;
@@ -47,7 +50,7 @@ public class User {
     private List<UserSubject> userSubjects;
 
     @Builder
-    public User(Long platformUserId, String platform, String nickname, Boolean isOnboardingComplete, Long userLevel) {
+    public User(Long platformUserId, String platform, String nickname, Boolean isOnboardingComplete, int userLevel) {
         this.platformUserId = platformUserId;
         this.platform = platform;
         this.nickname = nickname;
@@ -64,5 +67,58 @@ public class User {
         if(isOnboardingComplete != null) {
             this.isOnboardingComplete = isOnboardingComplete;
         }
+    }
+
+    /**
+     * 뱃지 관련 필드들
+     */
+    @Column(name = UserTableConstants.COLUMN_FIRST_STUDY_COMPLETED)
+    private LocalDateTime firstStudyCompletedAt = null; // 빵 굽기 시작 획득 여부 (맨 처음으로 '학습 완료')
+
+    @Column(name = UserTableConstants.COLUMN_FIRST_TODAY_TASKS_COMPLETED)
+    private LocalDateTime allTasksCompletedAt = null; // 오늘의 빵 완판 획득 여부 (맨 처음으로 '오늘 할 일'을 모두 완료)
+
+    @Column(name = UserTableConstants.COLUMN_HAS_MASS_BAKING_BREAD_BADGE)
+    private LocalDateTime hasMassBakingBreadBadge = null; // 빵 대량 생산 뱃지 획득 여부
+
+
+    @SuppressWarnings("FieldMayBeFinal")
+    @Column(name = UserTableConstants.COLUMN_TODAY_STUDY_COMPLETE_COUNT, nullable = false)
+    private int todayStudyCompleteCount = 0; // 하루 기준 "학습 완료" 횟수 (빵 대량 생산, 하루기준 '학습 완료' 3회 완료한 경우)
+
+    @Column(name = UserTableConstants.COLUMN_LAST_STUDY_COMPLETED_DATE)
+    private LocalDateTime lastStudyCompletedDate; // 마지막 "학습 완료" 날짜 : 날짜가 변경되면 todayStudyCompleteCount를 초기화하기 위해 사용
+
+    /**
+     * 뱃지 관련 로직들
+     */
+    public void markFirstStudyComplete() { // 맨 처음으로 '학습 완료' (빵 굽기 시작)
+        if (this.firstStudyCompletedAt == null) {
+            this.firstStudyCompletedAt = LocalDateTime.now();
+            this.point += 100;
+        }
+    }
+
+    public void markFirstTodayTasksCompletedAt() { // 맨 처름으로 '오늘 할 일'을 모두 완료 (오늘의 빵 완판)
+        if (this.allTasksCompletedAt == null) {
+            this.allTasksCompletedAt = LocalDateTime.now();
+            this.point += 200;
+        }
+    }
+
+    public void markHasMassBakingBreadBadge() { // '빵 대량 생산' 뱃지 획득하면 업데이트
+        if (this.hasMassBakingBreadBadge == null) {
+            this.hasMassBakingBreadBadge = LocalDateTime.now();
+            this.point += 50;
+        }
+    }
+
+    public void incrementTodayStudyCompleteCount() {
+        LocalDateTime now = LocalDateTime.now();
+        if (lastStudyCompletedDate == null || !lastStudyCompletedDate.toLocalDate().isEqual(LocalDateTime.now().toLocalDate())) {
+            this.todayStudyCompleteCount = 0; // 날짜가 바뀌면 초기화
+        }
+        this.todayStudyCompleteCount++;
+        this.lastStudyCompletedDate = now;
     }
 }
