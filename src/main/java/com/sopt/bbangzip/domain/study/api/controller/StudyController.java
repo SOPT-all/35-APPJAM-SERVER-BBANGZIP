234a92d93e407e17c4c0e498fcbc4cdc5bdb71c2
yeeun0702 <kimyeeun0702@naver.com>
2025-01-16 18:50:07 +0900

    ✨ [feat] #43 API 명세에 따른 성공 및 실패 응답 처리 로직 구현

--- src/main/java/com/sopt/bbangzip/domain/study/api/controller/StudyController.java
+++ src/main/java/com/sopt/bbangzip/domain/study/api/controller/StudyController.java
@@ -1,6 +1,14 @@
 package com.sopt.bbangzip.domain.study.api.controller;
 
+import com.sopt.bbangzip.common.annotation.UserId;
+import com.sopt.bbangzip.domain.study.api.dto.request.StudyCreateRequestDto;
+import com.sopt.bbangzip.domain.study.api.dto.response.StudyCreateResponseDto;
+import com.sopt.bbangzip.domain.study.service.StudyService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +16,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1")
 @RequiredArgsConstructor
 public class StudyController {
+    private final StudyService studyService;
+
+    @PostMapping("/studies")
+    public ResponseEntity<?> createStudy(
+            @UserId final Long userId,
+            @RequestBody @Valid final StudyCreateRequestDto studyCreateRequestDto
+    ) {
+        StudyCreateResponseDto responseDto = studyService.createStudy(userId, studyCreateRequestDto);
+        return ResponseEntity.ok(responseDto);
+    }
 }
