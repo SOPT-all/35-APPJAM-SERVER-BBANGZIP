d775a7a43eb60dae2d45766e5f64b1d77cf10475
yeeun0702 <kimyeeun0702@naver.com>
2025-01-17 01:54:34 +0900

    ✨ [feat] #43 ResponseEntity 제네릭 타입 명시

--- src/main/java/com/sopt/bbangzip/domain/study/api/controller/StudyController.java
+++ src/main/java/com/sopt/bbangzip/domain/study/api/controller/StudyController.java
@@ -1,11 +1,13 @@
 package com.sopt.bbangzip.domain.study.api.controller;
 
 import com.sopt.bbangzip.common.annotation.UserId;
+import com.sopt.bbangzip.common.dto.ResponseDto;
 import com.sopt.bbangzip.domain.study.api.dto.request.StudyCreateRequestDto;
 import com.sopt.bbangzip.domain.study.api.dto.response.StudyCreateResponseDto;
 import com.sopt.bbangzip.domain.study.service.StudyService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,11 +21,11 @@ public class StudyController {
     private final StudyService studyService;
 
     @PostMapping("/studies")
-    public ResponseEntity<?> createStudy(
+    public ResponseEntity<ResponseDto<StudyCreateResponseDto>> createStudy(
             @UserId final Long userId,
             @RequestBody @Valid final StudyCreateRequestDto studyCreateRequestDto
     ) {
         StudyCreateResponseDto responseDto = studyService.createStudy(userId, studyCreateRequestDto);
-        return ResponseEntity.ok(responseDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(ResponseDto.success(responseDto));
     }
 }
