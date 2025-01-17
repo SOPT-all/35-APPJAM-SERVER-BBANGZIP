59acc4457b0232008d4e71f1ff7f9d016d48423c
choyeongju <jjudy1301@naver.com>
2025-01-17 23:22:53 +0900

    :sparkles: [feat] #55 상수명 삭제

--- src/main/java/com/sopt/bbangzip/common/constants/entity/UserTableConstants.java
+++ src/main/java/com/sopt/bbangzip/common/constants/entity/UserTableConstants.java
@@ -11,8 +11,8 @@ public class UserTableConstants {
     public static final String COLUMN_USER_LEVEL = "user_level";
     public static final String COLUMN_CREATED_AT = "created_at";
     public static final String COLUMN_UPDATED_AT = "updated_at";
-    public static final String COLUMN_FIRST_STUDY_COMPLETED = "first_study_completed_at";
-    public static final String COLUMN_FIRST_TODAY_TASKS_COMPLETED = "first_today_tasks_completed_at";
+    public static final String COLUMN_FIRST_STUDY_COMPLETED_AT = "first_study_completed_at";
+    public static final String COLUMN_FIRST_TODAY_TASKS_COMPLETED_AT = "first_today_tasks_completed_at";
     public static final String COLUMN_TODAY_STUDY_COMPLETE_COUNT = "today_study_complete_count";
     public static final String COLUMN_LAST_STUDY_COMPLETED_DATE = "last_study_completed_date";
     public static final String COLUMN_HAS_MASS_BAKING_BREAD_BADGE = "has_mass_baking_bread_badge";
