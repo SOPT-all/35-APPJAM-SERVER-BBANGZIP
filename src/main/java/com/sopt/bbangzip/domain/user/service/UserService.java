d7ba61efe2270a714ffea7d3efd91428cacfb5c9
yeeun0702 <kimyeeun0702@naver.com>
2025-01-18 07:11:19 +0900

    ✨ [feat] #57 마이페이지 상태 확인 API 구현

--- src/main/java/com/sopt/bbangzip/domain/user/service/UserService.java
+++ src/main/java/com/sopt/bbangzip/domain/user/service/UserService.java
@@ -1,11 +1,30 @@
 package com.sopt.bbangzip.domain.user.service;
 
+import com.sopt.bbangzip.domain.user.api.dto.UserLevelResponseDto;
+import com.sopt.bbangzip.domain.user.entity.User;
+import com.sopt.bbangzip.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserService {
+
+    private final UserRetriever userRetriever;
+    private final UserLevelCalculator userLevelCalculator;
+    private final UserRepository userRepository;
+
+    @Transactional
+    public UserLevelResponseDto updateAndGetUserLevelStatus(Long userId) {
+        User user = userRetriever.findByUserId(userId);
+        // 새로운 레벨 계산 및 업데이트
+        int newLevel = userLevelCalculator.calculateLevel(user.getPoint());
+        user.updateUserLevel(newLevel);
+        userRepository.save(user);
+        return new UserLevelResponseDto(user.getUserLevel(), user.getBadgeCount(), user.getPoint());
+    }
+
 }
