dffe62130e475d3f17fb71b989364846e83c5523
yeeun0702 <kimyeeun0702@naver.com>
2025-01-18 22:33:41 +0900

    ✨ [feat] #59 뱃지 해금 여부 및 뱃지 카테고리 구현

--- src/main/java/com/sopt/bbangzip/domain/badge/Badge.java
+++ src/main/java/com/sopt/bbangzip/domain/badge/Badge.java
@@ -1,5 +1,7 @@
 package com.sopt.bbangzip.domain.badge;
 
+import com.sopt.bbangzip.domain.user.entity.User;
+
 import java.util.List;
 
 public interface Badge {
@@ -8,4 +10,6 @@ public interface Badge {
     int getReward(); // 리워드 반환
     List<String> getHashTags(); // 뱃지 관련 해시태그 반환
     String getImage(); // 뱃지 이미지 반환
+    String getCategory(); // 뱃지 카테고리 반환
+    Boolean isLocked(User user); // 잠금 여부 반환
 }
--- src/main/java/com/sopt/bbangzip/domain/badge/BadgeCondition.java
+++ src/main/java/com/sopt/bbangzip/domain/badge/BadgeCondition.java
@@ -2,6 +2,7 @@ package com.sopt.bbangzip.domain.badge;
 
 import com.sopt.bbangzip.domain.user.entity.User;
 
+@FunctionalInterface
 public interface BadgeCondition {
     boolean isEligible(User user); // 조건 판단 메서드
 }
