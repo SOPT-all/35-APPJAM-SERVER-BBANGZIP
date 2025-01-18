3cbf58bedafb4c020550b0c2942e80bd604f9673
yeeun0702 <kimyeeun0702@naver.com>
2025-01-19 05:55:52 +0900

    ✏️ [fix] #68 공부 조각 삭제하기 API 수정

---
+++ src/main/java/com/sopt/bbangzip/domain/piece/service/PieceRemover.java
@@ -0,0 +1,19 @@
+package com.sopt.bbangzip.domain.piece.service;
+
+import com.sopt.bbangzip.domain.piece.entity.Piece;
+import com.sopt.bbangzip.domain.piece.repository.PieceRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class PieceRemover {
+
+    private final PieceRepository pieceRepository;
+
+    public void removePieces(List<Piece> pieces) {
+        pieceRepository.deleteAll(pieces);
+    }
+}
