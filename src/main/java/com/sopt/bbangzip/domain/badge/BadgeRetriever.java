0cab186b993d48213cd8c1a54cd95193a363b68c
choyeongju <jjudy1301@naver.com>
2025-01-19 00:57:02 +0900

    :sparkles: [feat] #42 대환오빠 코드리뷰

---
+++ src/main/java/com/sopt/bbangzip/domain/badge/BadgeRetriever.java
@@ -0,0 +1,23 @@
+package com.sopt.bbangzip.domain.badge;
+
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * 대환핑이 짜줌
+ * 나중에 이거 참고해서 시간 남으면 리팩 할 것.
+ */
+@Component
+public class BadgeRetriever {
+    private final List <Badge> badges;
+
+    public BadgeRetriever(List<Badge> badges) {
+        this.badges = badges;
+    }
+
+    public List<Badge> getBadges() {
+
+        return badges;
+    }
+}
