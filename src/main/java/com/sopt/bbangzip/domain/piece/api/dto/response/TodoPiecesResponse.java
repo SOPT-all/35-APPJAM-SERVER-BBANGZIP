d3300b9c04a41bee9e68a9d9f996ed82ddccb0d8
choyeongju <jjudy1301@naver.com>
2025-01-20 02:09:50 +0900

    :ambulance: [hotfix] 필드명 오타 수정

--- src/main/java/com/sopt/bbangzip/domain/piece/api/dto/response/TodoPiecesResponse.java
+++ src/main/java/com/sopt/bbangzip/domain/piece/api/dto/response/TodoPiecesResponse.java
@@ -8,7 +8,7 @@ import java.util.List;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Builder
 public record TodoPiecesResponse(
-        Integer toadyCount,
+        Integer todayCount,
         Integer completeCount,
         List<TodoPieceDto> todoPieceStoList,
         Integer pendingCount
