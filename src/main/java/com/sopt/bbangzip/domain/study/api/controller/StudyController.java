16c0d2392c5de7dbfc914c72ef0632e37529bd42
choyeongju <jjudy1301@naver.com>
2025-01-21 07:43:03 +0900

    :recycle: [fix] #87 빵집 오픈 준비 중 뱃지 반환 로직 추가

--- src/main/java/com/sopt/bbangzip/domain/study/api/controller/StudyController.java
+++ src/main/java/com/sopt/bbangzip/domain/study/api/controller/StudyController.java
@@ -2,14 +2,15 @@ package com.sopt.bbangzip.domain.study.api.controller;
 
 import com.sopt.bbangzip.common.annotation.UserId;
 import com.sopt.bbangzip.common.dto.ResponseDto;
+import com.sopt.bbangzip.domain.badge.api.dto.response.BadgeResponse;
 import com.sopt.bbangzip.domain.study.api.dto.request.StudyCreateRequestDto;
+import com.sopt.bbangzip.domain.study.api.dto.response.CreateStudyResponse;
 import com.sopt.bbangzip.domain.study.service.StudyService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -18,11 +19,11 @@ public class StudyController {
     private final StudyService studyService;
 
     @PostMapping("/studies")
-    public ResponseEntity<ResponseDto<List<Object>>> createStudy(
+    public ResponseEntity<CreateStudyResponse> createStudy(
             @UserId final long userId,
             @RequestBody @Valid final StudyCreateRequestDto studyCreateRequestDto
     ) {
-        studyService.createStudy(userId, studyCreateRequestDto);
-        return ResponseEntity.ok(ResponseDto.success(null));
+        CreateStudyResponse response = studyService.createStudy(userId, studyCreateRequestDto);
+        return ResponseEntity.ok(ResponseDto.success(response).data());
     }
 }
