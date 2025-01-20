f7e8eb275d77b3d5f5fe2296d01706b9460c6da0
choyeongju <jjudy1301@naver.com>
2025-01-21 07:00:25 +0900

    :recycle: [fix] #87 뱃지 추가 및 패키지 이동

--- src/main/java/com/sopt/bbangzip/domain/badge/api/dto/response/BadgeResponse.java
+++ src/main/java/com/sopt/bbangzip/domain/badge/api/dto/response/BadgeResponse.java
@@ -1,7 +1,10 @@
 package com.sopt.bbangzip.domain.badge.api.dto.response;
 
+import lombok.Builder;
+
 import java.util.List;
 
+@Builder
 public record BadgeResponse(
         String badgeName,
         String badgeImage,
