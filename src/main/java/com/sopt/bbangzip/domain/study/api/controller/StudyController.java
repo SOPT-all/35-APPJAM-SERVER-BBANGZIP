bd155acbb5a0bea82a3039028730c542c89c9a20
choyeongju <jjudy1301@naver.com>
2025-01-21 03:06:33 +0900

    :recycle: [fix] #93 204 -> 200

--- src/main/java/com/sopt/bbangzip/domain/study/api/controller/StudyController.java
+++ src/main/java/com/sopt/bbangzip/domain/study/api/controller/StudyController.java
@@ -1,12 +1,15 @@
 package com.sopt.bbangzip.domain.study.api.controller;
 
 import com.sopt.bbangzip.common.annotation.UserId;
+import com.sopt.bbangzip.common.dto.ResponseDto;
 import com.sopt.bbangzip.domain.study.api.dto.request.StudyCreateRequestDto;
 import com.sopt.bbangzip.domain.study.service.StudyService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -15,11 +18,11 @@ public class StudyController {
     private final StudyService studyService;
 
     @PostMapping("/studies")
-    public ResponseEntity<Void> createStudy(
+    public ResponseEntity<ResponseDto<List<Object>>> createStudy(
             @UserId final long userId,
             @RequestBody @Valid final StudyCreateRequestDto studyCreateRequestDto
     ) {
         studyService.createStudy(userId, studyCreateRequestDto);
-        return ResponseEntity.noContent().build();
+        return ResponseEntity.ok(ResponseDto.success(List.of()));
     }
 }
