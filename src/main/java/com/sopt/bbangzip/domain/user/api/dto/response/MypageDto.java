5f188ab95edf1e550ba46ba4fda086e080945072
yeeun0702 <kimyeeun0702@naver.com>
2025-01-22 05:33:44 +0900

    ✏️ [fix] #90 들여쓰기 수정

--- src/main/java/com/sopt/bbangzip/domain/user/api/dto/response/MypageDto.java
+++ src/main/java/com/sopt/bbangzip/domain/user/api/dto/response/MypageDto.java
@@ -1,11 +1,14 @@
 package com.sopt.bbangzip.domain.user.api.dto.response;
 
+import java.util.List;
+
 public record MypageDto(
         int level,       // 사용자 레벨
         int badgeCounts, // 뱃지 개수
         int reward,       // 보상 포인트
         int maxReward,      // 현재 레벨의 최대 포인트
-        java.util.List<LevelDetail> levelDetails) {
+        List<LevelDetail> levelDetails
+) {
     public record LevelDetail(
             int level,               // 레벨
             String levelName,
