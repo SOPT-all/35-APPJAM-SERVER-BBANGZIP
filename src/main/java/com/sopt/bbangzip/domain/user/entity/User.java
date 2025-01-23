2a67f93f2d916ed35e8d1c10cb610e7886b8f863
yeeun0702 <kimyeeun0702@naver.com>
2025-01-23 18:26:30 +0900

    ✏️ [fix] #134 유저 레벨 1로 설정한 부분 Builder에서 제거

--- src/main/java/com/sopt/bbangzip/domain/user/entity/User.java
+++ src/main/java/com/sopt/bbangzip/domain/user/entity/User.java
@@ -38,7 +38,7 @@ public class User {
     private int point = 0;
 
     @Column(name = UserTableConstants.COLUMN_USER_LEVEL, nullable = false)
-    private int userLevel;
+    private int userLevel = 1;
 
     @Column(name = UserTableConstants.COLUMN_CREATED_AT, nullable = false, updatable = false)
     private LocalDateTime createdAt;
@@ -50,12 +50,11 @@ public class User {
     private List<UserSubject> userSubjects;
 
     @Builder
-    public User(Long platformUserId, String platform, String nickname, Boolean isOnboardingComplete, int userLevel) {
+    public User(Long platformUserId, String platform, String nickname, Boolean isOnboardingComplete) {
         this.platformUserId = platformUserId;
         this.platform = platform;
         this.nickname = nickname;
         this.isOnboardingComplete = isOnboardingComplete;
-        this.userLevel = userLevel;
         this.createdAt = LocalDateTime.now();
         this.updatedAt = LocalDateTime.now();
     }
