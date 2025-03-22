a68d8dd47c900140c77f83918636159c50acb518
choyeongju <jjudy1301@naver.com>
2025-03-22 16:56:03 +0900

    :recycle: [refac] 응답 코드 String 에서 int 로 변경

--- src/main/java/com/sopt/bbangzip/common/dto/ResponseDto.java
+++ src/main/java/com/sopt/bbangzip/common/dto/ResponseDto.java
@@ -5,12 +5,12 @@ import com.sopt.bbangzip.common.exception.code.BbangzipErrorCode;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public record ResponseDto<T> (
-        String code,
+        int code,
         T data,
         String message
 ) {
     public static <T> ResponseDto<T> success(final T data) {
-        return new ResponseDto<>("success", data, null);
+        return new ResponseDto<>(0, data, null);
     }
 
     public static <T> ResponseDto<T> fail(BbangzipErrorCode code) {
